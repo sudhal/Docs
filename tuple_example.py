10ff9024f8b10a03f2fc4008bf0138369412f6b7
Sudha <suda.lukka@gmail.com>
2018-08-28 21:45:51 +0300

    test

---
+++ tuple_example.py
@@ -0,0 +1,33 @@
+#list examples
+l=('fish','swan', 'gellifish')
+print(l)
+# in tuple we can't chnge the order,In Python tuples are written with round brackets.
+
+
+l=('fish','swan', 'gellifish')
+print(l[1])
+
+#ans=swan
+
+
+reverse a yuple:
+
+l=('fish','swan', 'gellifish')
+y=reversed(l)
+print(tuple(y))
+#result:('gellifish', 'swan', 'fish')
+
+
+
+)
+
+
+t = ('oh', 'my', 'tuple')
+print(t)
+
+
+
+
+
+
+
