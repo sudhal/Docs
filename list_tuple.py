b42a84bcfe7a34ebda1524d03cd6d9bcb9600375
Sudha <suda.lukka@gmail.com>
2018-08-31 22:30:42 +0300

    test

---
+++ list_tuple.py
@@ -0,0 +1,29 @@
+
+#Data Type conversions
+#List to tuple
+l=['a','b','c']
+s=tuple(l)
+print(s)
+
+#string to list
+
+s="hello"
+m=list(s)
+print(m)
+
+s="hello"
+k=s.split()
+print(k)
+
+#list to string
+nationality=['i','am','indian']
+c=''.join(nationality)
+print(c)
+
+
+
+
+
+
+
+
