c559cdaa3059d8210e36f166abf84d2d3dd9ceee
Sudha <suda.lukka@gmail.com>
2018-09-01 22:46:11 +0300

    test

---
+++ control flow _statements.py
@@ -0,0 +1,19 @@
+
+#control flow statements means flow of execution
+#If, elif, ifelseladder, nestedif
+
+l= range(10)
+for x in l:
+	if x%2==0:
+		print "This is a even number"
+		print x
+	else:
+		print "this is a odd number"
+		print x
+
+
+
+
+
+
+
---
+++ se.py
@@ -0,0 +1,11 @@
+l=[2,4,8,5,2,5,6,3]
+
+
+for i in l:
+	if i==2:
+		print i
+		print " found  2 ,this is correct"
+	else:
+		print i
+		print "we dint find 2 , this is not correct "
+		
